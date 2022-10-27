8d28f499fce5d431108e819821aea632a9545848
JuanLeonelC <juanleonelcd@gmail.com>
2022-10-27 19:17:55 -0300

    desafio-4

--- server.js
+++ server.js
@@ -1,32 +1,4 @@
-const express = require('express')
-const fs = require('fs')
-
-require('dotenv').config()
-
-const app = express()
-
-app.get('/',(_req, res)=>{
-    res.status(202).send('<h1>Hola</h1>')
-})
-app.get('/ping',(_req, res)=>{
-    res.status(202).send('<h1>pong</h1>')
-})
-
-app.get('/productos',(_req, res)=>{
-        const data = fs.readFileSync("./productos.txt" , 'utf-8')
-        let productos = data
-    res.status(202).json(productos)
-})
-
-app.get('/productoRandom',(_req, res)=>{
-    const data = fs.readFileSync("./productos.txt" , 'utf-8')
-    const jsonData = JSON.parse(data)
-    const random = Math.floor(Math.random()* jsonData.length)
-    const object = jsonData.find(obj => obj.id === random);
-    res.status(202).json(object)
-})
-
-
+const app = require("./app");
 
 const PORT = process.env.PORT
 
